c3ee2638f76b52d35889b4dae8a9f32c6452ecce
gaoteng-top <1531909499@qq.com>
2019-11-15 17:02:49 +0800

    gt

--- src/store/allshop/index.js
+++ src/store/allshop/index.js
@@ -1,1 +1,36 @@
-index.js
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+Vue.use(Vuex);
+ 
+let state = {
+   count:1
+}
+
+
+let actions = {
+   
+}
+
+
+let mutations = {
+    
+}
+
+
+
+let getters = {
+    completedCountChange(state){
+        return state.count * 2 +'位';
+    }
+}
+
+const store = new Vuex.Store({
+    state,
+    mutations,
+    getters,
+    actions,
+})
+
+
+export default store
